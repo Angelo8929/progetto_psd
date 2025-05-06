dab91fd7d2997e8d318eb52c5ec224bf841b13fc
TeoWoW05 <Matteotar89@gmail.com>
2025-05-06 17:19:19 +0200

    Aggiornamento Lezioni.c e Stesura Bozza Casi di Test

--- clienti.h
+++ clienti.h
@@ -2,7 +2,8 @@ typedef struct hash *hashtable;
 
 hashtable newHashtable(int size);
 int aggiungiCliente(hashtable h, struct Cliente cliente);
-// struct Cliente hashDelete(hashtable h, int key);
-// void destroyHashtable(hashtable h);
-// static void deleteList(struct Cliente *c);
-// int hashFun(int k, int m);
+struct Cliente newCliente(int id, char* nome, char* cognome);
+struct Cliente *hashDelete(hashtable h, int key);
+void destroyHashtable(hashtable h);
+ static void deleteList(struct Cliente *c);
+ int hashFun(int k, int m);
