3e834d35593e380d715aa8512098429f14d75ac0
Angelo <angeloverolla8@gmail.com>
2025-05-17 21:21:48 +0200

    risolto bug in utils.h riguardante le inclusioni duplicate a causa di conflitto precedente

--- utils.h
+++ utils.h
@@ -1,25 +1,8 @@
 #include "clienti.h"
 #include "lezioni.h"
 #include "prenotazioni.h"
-#include <stdlib.h>
-#include <stdio.h>
-
-#define MAX 100
-
-LezioniList carica_lezioni_file(char *nome_file)
-{
-    int id_lezione;
-    char nome[MAX];
-    char istruttore[MAX];
-    int posti_occupati;
-    char orario[6];
-    FILE *fp = fopen(nome_file, "r");
-}
-#include "clienti.h"
-#include "lezioni.h"
-#include "prenotazioni.h"
 #include "abbonamenti.h"
 
 void caricaClienti(hashtable h, char *filename);
-// LezioniList caricaLezioni(char *filename);
-// PrenotazioniList caricaPrenotazioni(char *filename, PrenotazioniList pList,hashtable h,LezioniList lList);
+LezioniList caricaLezioni(char *filename);
+PrenotazioniList caricaPrenotazioni(char *filename, PrenotazioniList prenotazioni, hashtable h, LezioniList lezioni);
